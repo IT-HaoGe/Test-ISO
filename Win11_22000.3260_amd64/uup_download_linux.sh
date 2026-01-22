1967b1575beaf4e423b4ffae2bea9b5f78ca119c
IT豪哥 <130531658+IT-HaoGe@users.noreply.github.com>
2026-01-23 03:45:30 +0800

    update

--- Win11_22000.3260_amd64/uup_download_linux.sh
+++ Win11_22000.3260_amd64/uup_download_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Generated on 2026-01-22 18:26:26 GMT
+#Generated on 2026-01-22 19:35:45 GMT
 
 # Proxy configuration
 # If you need to configure a proxy to be able to connect to the internet,
@@ -47,7 +47,7 @@ fi
 
 echo ""
 echo "Retrieving aria2 script for the UUP set..."
-aria2c --no-conf --console-log-level=warn --log-level=info --log="aria2_download.log" -o"$tempScript" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=35630b7b-4509-45b6-83a1-d4c75d5aa9b6&pack=zh-tw&edition=professional&aria2=2"
+aria2c --no-conf --console-log-level=warn --log-level=info --log="aria2_download.log" -o"$tempScript" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=35630b7b-4509-45b6-83a1-d4c75d5aa9b6&pack=en-us&edition=professional&aria2=2"
 if [ $? != 0 ]; then
   echo "Failed to retrieve aria2 script"
   exit 1
