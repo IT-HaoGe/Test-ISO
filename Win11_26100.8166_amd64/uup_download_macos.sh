eb973e027e05592a6ab35a99fa729fa38ea65963
IT豪哥 <130531658+IT-HaoGe@users.noreply.github.com>
2026-03-27 10:06:36 +0800

    update

--- Win11_26100.8166_amd64/uup_download_macos.sh
+++ Win11_26100.8166_amd64/uup_download_macos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Generated on 2026-03-23 05:08:27 GMT
+#Generated on 2026-03-27 02:04:25 GMT
 
 # Proxy configuration
 # If you need to configure a proxy to be able to connect to the internet,
@@ -47,7 +47,7 @@ fi
 
 echo ""
 echo "Retrieving aria2 script for the UUP set..."
-aria2c --no-conf --console-log-level=warn --log-level=info --log="aria2_download.log" -o"$tempScript" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=d2ed457a-318d-40be-85dd-61ae905f59ae&pack=zh-cn&edition=professional%3Bcorecountryspecific%3Bcore&aria2=2"
+aria2c --no-conf --console-log-level=warn --log-level=info --log="aria2_download.log" -o"$tempScript" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=a0ea85e8-4330-43ef-a88a-d982ae96ea3a&pack=zh-cn&edition=professional%3Bcorecountryspecific%3Bcore&aria2=2"
 if [ $? != 0 ]; then
   echo "Failed to retrieve aria2 script"
   exit 1
